e9e41c4633d3fc53716ae6802d3be94933660baf
Amash10 <amashansari74@gmail.com>
2025-07-03 15:40:03 +0530

    whatsapp

--- controllers/user_data.js
+++ controllers/user_data.js
@@ -1,6 +1,9 @@
 const UserDataService = require("../service/user_data");
 const UserDataValidations = require("../validator/user_data");
-
+const fs = require("fs");
+const path = require("path");
+const qrImagePath = path.join(__dirname, '../whatsapp_qr.png');
+const qrImageBase64 = fs.readFileSync(qrImagePath, { encoding: 'base64' });
 /////////////--------------------------------//////////////////////////////
 // Get All UserDatas Controller
 ///////////////------------------------------//////////////////////////////
@@ -38,7 +41,33 @@ addUserData = async (req, res, next) => {
     }
 };
 
+
+getQrCodeForWhatsappWeb = async (req, res, next) => {
+  try {
+    const baseDir = path.resolve(__dirname, "../"); 
+    const authPath = path.join(baseDir, ".wwebjs_auth");
+    const cachePath = path.join(baseDir, ".wwebjs_cache");
+
+    // Helper to delete folders recursively if they exist
+    const deleteFolderIfExists = (folderPath) => {
+      if (fs.existsSync(folderPath)) {
+        fs.rmSync(folderPath, { recursive: true, force: true });
+        console.log(`Deleted folder: ${folderPath}`);
+      }
+    };
+
+    deleteFolderIfExists(authPath);
+    deleteFolderIfExists(cachePath);
+
+    const qrData = await UserDataService.getQrCodeForWhatsappWeb();
+    res.status(200).json({ status: 1, qr: qrData, qrimage: qrImagePath });
+  } catch (error) {
+    res.status(500).json({ status: 0, message: "Failed to generate QR", error });
+  }
+};
+
 module.exports = {
     getAllUserData,
     addUserData,
+    getQrCodeForWhatsappWeb
 };
