be287c544c2c747cbfa3a1361bd09f579fec7f2b
Amash10 <amashansari74@gmail.com>
2025-07-04 15:25:55 +0530

    main commit

--- controllers/user_data.js
+++ controllers/user_data.js
@@ -1,71 +1,47 @@
 const UserDataService = require("../service/user_data");
 const UserDataValidations = require("../validator/user_data");
-const fs = require("fs");
-const path = require("path");
+
+
 /////////////--------------------------------//////////////////////////////
 // Get All UserDatas Controller
 ///////////////------------------------------//////////////////////////////
 getAllUserData = async (req, res, next) => {
-    try {
-        const resp = await UserDataService.getAllUserData(req);
-        res.status(200).json(resp);
-    } catch (error) {
-        console.log(error);
-        next(error);
-    }
+  try {
+    const resp = await UserDataService.getAllUserData(req);
+    res.status(200).json(resp);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
+
 
 /////////////--------------------------------//////////////////////////////
 // Add UserData Controller []
 ///////////////------------------------------//////////////////////////////
 addUserData = async (req, res, next) => {
-    try {
-        let { first_name, last_name, birth_date } = req.body;
-
-        let validateData = await UserDataValidations.addValidation.validate(req.body);
-        if (validateData && validateData.error) {
-            return res.status(422).json({ error: validateData.error.message });
-        }
-        const resp = await UserDataService.postUserData({
-            req,
-            first_name,
-            last_name,
-            birth_date,
-        });
-        res.status(200).json(resp);
-    } catch (error) {
-        console.log(error);
-        next(error);
-    }
-};
-
-
-getQrCodeForWhatsappWeb = async (req, res, next) => {
   try {
-    const baseDir = path.resolve(__dirname, "../"); 
-    const authPath = path.join(baseDir, ".wwebjs_auth");
-    const cachePath = path.join(baseDir, ".wwebjs_cache");
+    let { first_name, last_name, birth_date } = req.body;
 
-    // Helper to delete folders recursively if they exist
-    const deleteFolderIfExists = (folderPath) => {
-      if (fs.existsSync(folderPath)) {
-        fs.rmSync(folderPath, { recursive: true, force: true });
-        console.log(`Deleted folder: ${folderPath}`);
-      }
-    };
-
-    deleteFolderIfExists(authPath);
-    deleteFolderIfExists(cachePath);
-
-    const qrData = await UserDataService.getQrCodeForWhatsappWeb();
-    res.status(200).json({ status: 1, qr: qrData});
+    let validateData = await UserDataValidations.addValidation.validate(req.body);
+    if (validateData && validateData.error) {
+      return res.status(422).json({ error: validateData.error.message });
+    }
+    const resp = await UserDataService.postUserData({
+      req,
+      first_name,
+      last_name,
+      birth_date,
+    });
+    res.status(200).json(resp);
   } catch (error) {
-    res.status(500).json({ status: 0, message: "Failed to generate QR", error });
+    console.log(error);
+    next(error);
   }
 };
 
+
 module.exports = {
-    getAllUserData,
-    addUserData,
-    getQrCodeForWhatsappWeb
+  getAllUserData,
+  addUserData
 };
--- routes/user_data.js
+++ routes/user_data.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUserData, addUserData, getQrCodeForWhatsappWeb } = require("../controllers/user_data");
+
+const { getAllUserData, addUserData } = require("../controllers/user_data");
+
 
 router.route("/api/getAllUserDetail").get(getAllUserData);
 router.route("/api/postUserDetail").post(addUserData);
-router.route("/api/GetQr").get(getQrCodeForWhatsappWeb);
+
 
 module.exports = router;
